235b556f157e1376b2f76f69a6daacca5b79053c
bryce-guglietti <98065505+bryce-guglietti@users.noreply.github.com>
2022-08-17 19:45:09 -0400

    Create removeElement.py
    Remove Element problem from Leetcode

---
+++ removeElement.py
@@ -0,0 +1,15 @@
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
+        j = 0
+        
+        for i in nums:
+            if i != val:
+                nums[j] = i
+                j+=1
+        
+        return j
