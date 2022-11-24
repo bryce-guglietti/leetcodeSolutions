cfc0954d254bfd68e1433745ed98c4639b2e93e3
Bryce Guglietti <98065505+bryce-guglietti@users.noreply.github.com>
2022-11-24 17:53:14 -0500

    Update SearchInsertPosition.py

--- SearchInsertPosition.py
+++ SearchInsertPosition.py
@@ -4,16 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :type target: int
         :rtype: int
-        """
+        """        
         l, r = 0, len(nums)-1
         while l<=r:
-            mid = l + ( r-l )/2
-            if ( nums[mid] < target ):
-                l = mid + 1
-            elif ( nums [mid] > target ):
-                r = mid - 1
-            
+            mid = l + ( r-l )//2                
             if nums[mid] == target:
                 return mid
-            
+            elif ( nums[mid] < target ):
+                l = mid + 1
+            else:
+                r = mid - 1
+
         return l
