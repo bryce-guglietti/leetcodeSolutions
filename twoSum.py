da4a46293053c43ef2a3854f57f86fdb378a78e2
Bryce Guglietti <98065505+bryce-guglietti@users.noreply.github.com>
2022-11-25 15:17:29 -0500

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -15,3 +15,20 @@ class Solution(object):
             if complement in hashmap and hashmap[complement] != i:
                 return [i, hashmap[complement]]
         return None
+
+# another hashmap solution
+# class Solution(object):
+# def twoSum(self, nums, target):
+#     """
+#     :type nums: List[int]
+#     :type target: int
+#     :rtype: List[int]
+#     """
+#     hashmap = {}
+
+#     for i, n in enumerate(nums):
+#         val = target - n
+#         if val in hashmap:
+#             return [hashmap[val], i]
+#         else:
+#             hashmap[n] = i
